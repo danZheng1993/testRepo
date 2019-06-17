abe9523e42413b3d5fb9869fed64ecefbce72e2a
Daniel Zheng <dannyzheng1993@gmail.com>
2019-06-18 01:10:44 +0800

    Basic Functionality

--- src/views/main/components/PhotoGrid/index.js
+++ src/views/main/components/PhotoGrid/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { getPhotoResult } from 'store/selectors';
+
+import Photo from './Photo';
 
 const styles= {
   wrapper: {
     flex: 4,
     height: '100%',
+    overflowY: 'scroll',
   }
 }
 
-export default class PhotoGrid extends React.Component {
+class PhotoGrid extends React.Component {
+  renderPhoto = (photo) => {
+    return ( <Photo photo={photo} key={`photo_${photo.id}`} /> );
+  }
   render() {
+    const { photos } = this.props;
     return (
-      <div style={styles.wrapper}>PhotoGrid</div>
+      <div style={styles.wrapper}>
+        {photos.map(this.renderPhoto)}
+      </div>
     )
   }
 }
+
+const mapStateToProps = (state) => ({ ...getPhotoResult(state) });
+
+export default connect(mapStateToProps)(PhotoGrid);
